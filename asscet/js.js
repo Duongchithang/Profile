87a9773627766bd57d7267c2f82fb438ab78b286
Chithang0210 <78576625+Duongchithang@users.noreply.github.com>
2022-01-09 20:49:05 +0700

    .

--- asscet/js.js
+++ asscet/js.js
@@ -3,11 +3,12 @@ const Items = document.querySelectorAll('.select');
 const PlayMusic = document.querySelector('.Music');
 const Music = document.querySelector('.music');
 const CloseMusic = document.querySelector('.play-close');
+const PauseSong = document.querySelector('#song');
 PlayMusic.addEventListener('click', function() {
     Music.classList.add('play-music')
 
 })
 CloseMusic.addEventListener('click', function() {
     Music.classList.remove('play-music');
-
+    PauseSong.pause();
 })
